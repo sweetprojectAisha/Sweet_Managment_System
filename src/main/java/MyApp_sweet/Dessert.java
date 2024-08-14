589f032009425971d1c2250979f7a9071308564e
Farah <148708960+MahmoudFarah003@users.noreply.github.com>
2024-08-14 19:35:33 +0300

    Here Everything related to dessert attributes

---
+++ src/main/java/MyApp_sweet/Dessert.java
@@ -0,0 +1,45 @@
+package sweet.pal;
+
+public class Dessert
+{
+
+
+    private String dessertName;
+    private double price;
+    private String availability;
+    private String dietaryInfo;
+
+    // Getters and setters
+    public String getDessertName() {
+        return dessertName;
+    }
+
+    public void setDessertName(String dessertName) {
+        this.dessertName = dessertName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    public String getDietaryInfo() {
+        return dietaryInfo;
+    }
+
+    public void setDietaryInfo(String dietaryInfo) {
+        this.dietaryInfo = dietaryInfo;
+    }
+
+}
