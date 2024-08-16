5f76c6e8214176d6d3bb24bb82a69dd8702b3cdc
Amal-ALdeek <deekamal55@gmail.com>
2024-08-16 20:34:49 +0300

    Create EmailService.java

---
+++ src/main/java/MyApp_sweet/EmailService.java
@@ -0,0 +1,26 @@
+package Admin_helperClass;
+
+public class EmailService {
+
+    public EmailService() {
+    }
+
+    // Method to send a welcome email
+    public void sendWelcomeEmail(String email) {
+        boolean emailSent = simulateEmailSending(email, "Welcome to Our Service", "Dear user, welcome to our service!");
+
+        if (emailSent) {
+            System.out.println("Welcome email successfully sent to: " + email);
+        } else {
+            System.out.println("Failed to send welcome email to: " + email);
+        }
+    }
+
+    private boolean simulateEmailSending(String email, String subject, String body) {
+        System.out.println("Sending email to: " + email);
+        System.out.println("Subject: " + subject);
+        System.out.println("Body: " + body);
+
+        return true;
+    }
+}
