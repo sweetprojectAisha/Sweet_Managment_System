da928a492a80cb03d90efa1021778760c535331a
Amal-ALdeek <deekamal55@gmail.com>
2024-08-16 20:40:30 +0300

    Create StoreOwnerManager.java

---
+++ src/main/java/MyApp_sweet/StoreOwnerManager.java
@@ -0,0 +1,17 @@
+package Admin_helperClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreOwnerManager {
+    private List<StoreOwner> owners = new ArrayList<>();
+
+    public boolean addStoreOwner(String username, String email) {
+        StoreOwner newOwner = new StoreOwner(username, email);
+        return owners.add(newOwner);
+    }
+
+    public List<StoreOwner> getOwners() {
+        return owners;
+    }
+}
