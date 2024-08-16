d38645f533439ff8bf909e60e999db99cd9b18f0
Amal-ALdeek <deekamal55@gmail.com>
2024-08-16 20:32:37 +0300

    Create AdminService.java

---
+++ src/main/java/MyApp_sweet/AdminService.java
@@ -0,0 +1,22 @@
+package Admin_helperClass;
+
+public class AdminService {
+
+    private boolean loggedIn;
+
+    public boolean login() {
+        loggedIn = true;
+        return loggedIn;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public StoreOwnerAccount createStoreOwnerAccount(String username, String email) {
+        if (!loggedIn) {
+            throw new IllegalStateException("Admin is not logged in.");
+        }
+        return new StoreOwnerAccount(username, email);
+    }
+}
