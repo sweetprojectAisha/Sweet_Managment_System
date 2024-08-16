7cfb63cd1dfe741fa7b7ab603c6628ff4bf67cef
Amal-ALdeek <deekamal55@gmail.com>
2024-08-16 20:41:22 +0300

    Create StoreProfitService.java

---
+++ src/main/java/MyApp_sweet/StoreProfitService.java
@@ -0,0 +1,20 @@
+package Admin_helperClass;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StoreProfitService {
+
+    private Map<String, Double> storeProfits;
+
+    public StoreProfitService() {
+        storeProfits = new HashMap<>();
+        storeProfits.put("Store A", 12000.00);
+        storeProfits.put("Store B", 15000.00);
+        storeProfits.put("Store C", 18000.00);
+    }
+
+    public Map<String, Double> getStoreProfits() {
+        return storeProfits;
+    }
+}
