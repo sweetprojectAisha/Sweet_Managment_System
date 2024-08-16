d01bf171ca3b2eb181b69b26c86b37adb99fc6cb
Amal-ALdeek <deekamal55@gmail.com>
2024-08-16 20:43:48 +0300

    Create TopSellingItemsService.java

---
+++ src/main/java/MyApp_sweet/TopSellingItemsService.java
@@ -0,0 +1,27 @@
+package Admin_helperClass;
+
+import java.util.List;
+import java.util.Map;
+
+public class TopSellingItemsService {
+
+    public List<Map<String, Object>> getTopSellingItems(String storeId) {
+        return List.of(
+                Map.of(
+                        "productName", "Kunafa",
+                        "quantitiesSold", 120,
+                        "totalRevenue", 2400
+                ),
+                Map.of(
+                        "productName", "Baqlawa",
+                        "quantitiesSold", 85,
+                        "totalRevenue", 1700
+                ),
+                Map.of(
+                        "productName", "cookie",
+                        "quantitiesSold", 89,
+                        "totalRevenue", 1300
+                )
+        );
+    }
+}
