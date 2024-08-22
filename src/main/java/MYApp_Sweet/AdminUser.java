d4ee6c170e61de7e4b8af477e969c67034617110
AishaRiyad <s12113150@stu.najah.edu>
2024-08-22 22:30:50 +0300

    Update AdminUser.java

--- src/main/java/MYApp_Sweet/AdminUser.java
+++ src/main/java/MYApp_Sweet/AdminUser.java
@@ -1,17 +1,1 @@
-package MYApp_Sweet;
 
-public class AdminUser {
-    private boolean loggedIn;
-
-    public AdminUser() {
-        this.loggedIn = false;
-    }
-
-    public void logIn() {
-        this.loggedIn = true;
-    }
-
-    public boolean isLoggedIn() {
-        return loggedIn;
-    }
-}
